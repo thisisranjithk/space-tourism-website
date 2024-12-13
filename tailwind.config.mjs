3abbe401fec5fb186f96ddaec92a24e466c28712
Ranjith K <thisisranjithk@gmail.com>
2024-12-13 19:07:42 +0530

    tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,8 +8,22 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        lightBlue: "#D0D6F9",
+      },
+      backgroundImage: {
+        homeDesktop: "url('/assets/home/background-home-desktop.jpg')",
+        homeTablet: "url('/assets/home/background-home-tablet.jpg')",
+        homeMobile: "url('/assets/home/background-home-mobile.jpg')",
+        destinationDesktop:
+          "url('/assets/destination/background-destination-desktop.jpg')",
+        destinationTablet:
+          "url('/assets/destination/background-destination-tablet.jpg')",
+        destinationMobile:
+          "url('/assets/destination/background-destination-mobile.jpg')",
+      },
+      fontFamily: {
+        sans: ["Bellefair", "serif"],
+        barlowCondense: ["Barlow Condensed", "sans-serif"],
       },
     },
   },
