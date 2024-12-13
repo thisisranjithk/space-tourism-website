5eabb7e07b31428a558fd39cb1fe21b1d9dd6b62
Ranjith K <thisisranjithk@gmail.com>
2024-12-13 19:07:07 +0530

    constants data

---
+++ constants.js
@@ -0,0 +1,24 @@
+export const METADATA = {
+  author: "Ranjith K",
+  title: "Space Tourism",
+  description: "Space Tourism a Frontend-Mentor challenge",
+};
+
+export const MENULINKS = [
+  {
+    name: "Home",
+    ref: "",
+  },
+  {
+    name: "Destination",
+    ref: "destination",
+  },
+  {
+    name: "Crew",
+    ref: "crew",
+  },
+  {
+    name: "Technology",
+    ref: "technology",
+  },
+];
