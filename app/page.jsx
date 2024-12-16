4390002f7f46c93cd68a6b54ca5e1341ec81aa88
Ranjith K <thisisranjithk@gmail.com>
2024-12-16 19:02:42 +0530

    home page UI bug fix

--- app/page.jsx
+++ app/page.jsx
@@ -8,13 +8,13 @@ export default function Home() {
         id="home"
         className="flex container mx-auto items-center justify-center min-h-[80vh]"
       >
-        <div className="container flex flex-col lg:flex-row gap-12 mt-8">
+        <div className="flex flex-col lg:flex-row gap-12 mt-8">
           <div className="home-content flex-1 flex items-center lg:items-start flex-col gap-6">
             <h3 className="uppercase font-barlowCondense text-2xl font-light tracking-widest">
               So, you want to travel to
             </h3>
             <h1 className="uppercase text-8xl md:text-9xl">Space</h1>
-            <p className="max-w-lg  lg:max-w-xl text-center lg:text-left">
+            <p className="max-w-lg  lg:max-w-xl text-center lg:text-left text-lightBlue">
               Let’s face it; if you want to go to space, you might as well
               genuinely go to outer space and not hover kind of on the edge of
               it. Well sit back, and relax because we’ll give you a truly out of
