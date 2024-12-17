5c152a084c6027e178c29c945fdf8ed8ef3c5f89
Ranjith K <thisisranjithk@gmail.com>
2024-12-17 12:01:51 +0530

    navbar glass effect UI bug fix

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -9,9 +9,9 @@ const Navbar = ({ activeTab }) => {
       <header className="flex md:items-start lg:items-center justify-between h-[10vh] lg:h-[20vh] ml-[5%]">
         <div className="lg:flex-1 flex items-start md:items-center relative mt-6 ml-4 lg:mt-0 lg:ml-0">
           <img src="/assets/shared/logo.svg" alt="logo" />
-          <span className="hidden lg:block h-[1px] bg-white/[.20] w-[80%] z-20 absolute right-[-30px]"></span>
+          <span className="hidden lg:block h-[1px] bg-lightBlue/[.20] w-[80%] z-20 absolute right-[-30px]"></span>
         </div>
-        <nav className="hidden md:flex justify-center bg-white/[.10] lg:flex-1 backdrop-blur py-8 px-20">
+        <nav className="hidden md:flex justify-center bg-lightBlue/[.10] lg:flex-1 backdrop-blur py-8 px-20">
           <ul className={`navlinks`}>
             {MENULINKS.map((link, index) => (
               <li className="nav-link group" key={link.ref}>
